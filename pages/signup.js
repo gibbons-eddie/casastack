3dbc77ca845f67c998742ae3c265b2045dc00122
Eddie Gibbons <gibbons.eddie@ufl.edu>
2020-07-09 10:35:28 -0400

    changes to signup.js test

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,9 @@
 import Layout from "../components/homePage/Layout";
+import { Button, Form, Icon, Message, Segment } from 'semantic-ui-react'; // change
 
-const SignUp = (props) =><Layout>{`signup`}</Layout>;
+const SignUp = (props) => 
+<Layout>
+    {`signup`}
+</Layout>;
 
 export default SignUp;
