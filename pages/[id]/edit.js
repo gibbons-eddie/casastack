f8bd0d229eca9f4a4a390ac3bf8cfe4c83e4090e
Richard Clarke <ricky.jiangclarke@gmail.com>
2020-07-13 10:04:49 -0400

    View and update have been worked on

---
+++ pages/[id]/edit.js
@@ -0,0 +1,114 @@
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import fetch from 'isomorphic-unfetch';
+import { Button, Form, Loader } from 'semantic-ui-react';
+import { useRouter } from 'next/router';
+
+const EditListing = ({ listing }) => {
+    const [form, setForm] = useState({ service: listing.service, status: listing.status, location: listing.location, description: listing.description });
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [errors, setErrors] = useState({});
+    const router = useRouter();
+
+    useEffect(() => {
+        if (isSubmitting) {
+            if (Object.keys(errors).length === 0) {
+                updateListing();
+            }
+            else {
+                setIsSubmitting(false);
+            }
+        }
+    }, [errors])
+
+    const updateListing = async () => {
+        try {
+            const res = await fetch(`http://localhost:3000/api/listings/${router.query.id}`, {
+                method: 'PUT',
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(form)
+            })
+            router.push("/");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let errs = validate();
+        setErrors(errs);
+        setIsSubmitting(true);
+    }
+
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const validate = () => {
+        let err = {};
+
+        if (!form.service) {
+            err.service = 'Service is required';
+        }
+        if (!form.status) {
+            err.status = 'Status is required';
+        }
+        if (!form.location) {
+            err.location = 'Location is required';
+        }
+        if (!form.description) {
+            err.description = 'Description is required';
+        }
+
+        return err;
+    }
+
+    return (
+        <div className="form-container">
+            <h1>Update Note</h1>
+            <div>
+                {
+                    isSubmitting
+                        ? <Loader active inline='centered' />
+                        : <Form onSubmit={handleSubmit}>
+                            <Form.Input
+                                fluid
+                                error={errors.title ? { content: 'Please enter a title', pointing: 'below' } : null}
+                                label='Title'
+                                placeholder='Title'
+                                name='title'
+                                value={form.title}
+                                onChange={handleChange}
+                            />
+                            <Form.TextArea
+                                fluid
+                                label='Descriprtion'
+                                placeholder='Description'
+                                name='description'
+                                error={errors.description ? { content: 'Please enter a description', pointing: 'below' } : null}
+                                value={form.description}
+                                onChange={handleChange}
+                            />
+                            <Button type='submit'>Update</Button>
+                        </Form>
+                }
+            </div>
+        </div>
+    )
+}
+
+EditNote.getInitialProps = async ({ query: { id } }) => {
+    const res = await fetch(`http://localhost:3000/api/notes/${id}`);
+    const { data } = await res.json();
+
+    return { note: data }
+}
+
+export default EditNote;
