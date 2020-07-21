1910c68712c848ec50c4148d1a8c6c25faa42dfc
Eddie Gibbons <gibbons.eddie@ufl.edu>
2020-07-20 23:19:43 -0400

    replaced navbar with sidebar

--- components/homePage/Header.js
+++
@@ -1,9 +0,0 @@
-import homeStyles from './homePageStyles/Home.module.css'
-
-const Header = () => (
-    <div className={homeStyles.Header}>
-        Casastack
-    </div>
-);
-
-export default Header;
--- components/homePage/Layout.js
+++ components/homePage/Layout.js
@@ -1,23 +1,81 @@
-import Header from "./Header";
-import NavBar from "./NavBar";
 import Head from "next/head";
-
+import Link from 'next/link';
 import homeStyles from './homePageStyles/Home.module.css'
+import { Sidebar, Icon, Menu } from "semantic-ui-react";
 
 const Layout = (props) => (
+  
+  // page overlay, including header and side nav bar
   <div className={homeStyles.Layout}>
-
     <Head>
       <title>casastack</title>
       <meta charSet="utf-8"/>
     </Head>
 
-    <Header />
-    <div className={homeStyles.Content}>
-      {props.children}
+    <Sidebar
+        as={Menu}
+        direction='left'
+        icon='labeled'
+        inverted
+        vertical
+        visible
+        
+    >
+        
+        <Link href="/">
+          <Menu.Item as='a'>
+            <Icon
+              name="home"
+            />
+            Home
+          </Menu.Item>
+        </Link>
 
-    </div>
-    <NavBar />
+        <Link href="/login">
+          <Menu.Item as='a'>
+            <Icon
+              name="sign-in alternate"
+            />
+            Sign In
+          </Menu.Item>
+        </Link>
+
+        <Link href="/joblisting">
+          <Menu.Item as='a'>
+            <Icon
+              name="clipboard"
+            />
+            Listings
+          </Menu.Item>
+        </Link>
+
+        <Link href="/profile">
+          <Menu.Item as='a'>
+            <Icon
+              name="id card"
+            />
+            Profile
+          </Menu.Item>
+        </Link>
+
+        <Link href="/signup">
+          <Menu.Item as='a'>
+            <Icon
+              name="edit"
+            />
+            Sign Up
+          </Menu.Item>
+        </Link>
+
+    </Sidebar>
+      
+      <div className={homeStyles.Content}> 
+        <div className={homeStyles.Header}>
+          Casastack
+        </div>
+        {props.children}
+      </div>
+    
   </div>
 );
 
