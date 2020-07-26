1f137231513e16c92f5af90cb9ff983285af258c
Joseph Fauls <hnuisqt@gmail.com>
2020-07-25 20:38:15 -0400

    view listing page and functional buttons

---
+++ components/profilePage/ViewListings.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+import { Button, Card, Form, Segment, Container} from 'semantic-ui-react';
+
+
+const ViewListing = (props) => {
+    if (props.role ==="user") {
+        return(<div><Link href={`/profile/user/${props.email}`}>
+    <Button circular size='big' color='twitter' type='submit' style={{fontFamily: 'Montserrat', fontWeight: '350'}}
+        content='View my Listings'
+    />
+</Link></div>);
+    }
+    if (props.role === "volunteer") { return(<div></div>);}
+}
+
+export default ViewListing;
