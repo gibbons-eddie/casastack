074c537807acd2861aac2a28d848391adf1e83ca
Lucas Liseth <lucasl2000@gmail.com>
2020-08-06 20:12:40 -0500

    Added table to rewards page

--- pages/rewards.js
+++ pages/rewards.js
@@ -1,46 +1,49 @@
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
-import { Button, Card} from 'semantic-ui-react';
+import { Button, Card, Table } from 'semantic-ui-react';
 import jobListingStyle from '../components/joblistingsPage/jobListingPageStyles/joblisting.module.css'
 import baseURL from '../utils/baseURL';
+
+// const rewardsTable = () => (
+// );
 
 const rewards1 = ({ rewards, user }) => {
     const isRoot = user.role === 'root';
     const isAdmin = user.role === 'admin';
     const isVolunteer = user.role === 'volunteer';
-    
-    return(
+
+    return (
 
         <div className={jobListingStyle.jobListingsHeader}>
 
             <div className={jobListingStyle.listingTitle}>
-            Rewards
+                Rewards
             </div>
-            
-                <br></br>
-                <br></br>
 
-        <div className="grid wrapper">
+            <br></br>
+            <br></br>
 
-            {rewards.map(rewards => {
-                return (
-                    <div key={rewards._id}>
-                        <Card>
-                            <Card.Content>
-                                <Card.Header>
+            <div className="grid wrapper">
 
-                                    <Link href={`/rewardInfo/${rewards._id}`}>
-                                        <h1>{rewards.name}</h1>
-                                    </Link>
+                {rewards.map(rewards => {
+                    return (
+                        <div key={rewards._id}>
+                            <Card>
+                                <Card.Content>
+                                    <Card.Header>
 
-                                    <Link href={`/rewardInfo/${rewards._id}`}>
-                                        <h2>{rewards.points}</h2>
-                                    </Link>
-                                    
-                                </Card.Header>
-                            </Card.Content>
+                                        <Link href={`/rewardInfo/${rewards._id}`}>
+                                            <h1>{rewards.name}</h1>
+                                        </Link>
 
-                            <Card.Content extra>
+                                        <Link href={`/rewardInfo/${rewards._id}`}>
+                                            <h2>{rewards.points}</h2>
+                                        </Link>
+
+                                    </Card.Header>
+                                </Card.Content>
+
+                                <Card.Content extra>
                                     <div>
                                         <Button className="card-button" primary size="small">
                                             <Link href={`/rewardInfo/${rewards._id}`}>
@@ -49,29 +52,35 @@ const rewards1 = ({ rewards, user }) => {
                                         </Button>
                                         <br></br>
                                         <br></br>
-                                        
+
                                         {(isRoot || isAdmin) && (<Button className="card-button" primary size="small">
                                             <Link href={`/rewardInfo/${rewards._id}/edit`}>
                                                 <h3>Edit Reward</h3>
                                             </Link>
                                         </Button>)}
                                     </div>
-                                    
-                                    
-                            
 
-                            </Card.Content>
-                        </Card>
-                    </div>
-                )
-            })}
-        
-            
+
+
+
+                                </Card.Content>
+                            </Card>
+                        </div>
+                    )
+                })}
+            </div>
+            <Table celled padded>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell singleLine>User</Table.HeaderCell>
+                        <Table.HeaderCell>Number of Deliveries</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+            </Table>
+
         </div>
-        </div>
-    )
-    
-}
+    );
+};
 
 rewards1.getInitialProps = async () => {
     const res = await fetch(`${baseURL}/api/rewards`);
