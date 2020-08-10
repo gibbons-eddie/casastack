d5adabcd5eee0c5bff6c86cb15c07d45ac9b10d4
Eddie Gibbons <gibbons.eddie@ufl.edu>
2020-08-10 11:25:24 -0400

    layout fixes

--- components/homePage/Layout.js
+++ components/homePage/Layout.js
@@ -1,97 +1,134 @@
-import Head from 'next/head';
+import Head from "next/head";
 import Link from 'next/link';
 import homeStyles from './homePageStyles/Home.module.css'
 import { Sidebar, Icon, Menu } from "semantic-ui-react";
-import { handleLogout } from '../../utils/auth';
-
-function Layout({ children, user }) {
+import { handleLogout } from '../../utils/auth'
+function Layout ({children, user}) {
   const isRoot = user && user.role === 'root';
   const isAdmin = user && user.role === 'admin';
   const isVolunteer = user && user.role === 'volunteer';
   const isCustomer = isRoot || isAdmin || isVolunteer;
-
-  //const logo = require('../../static/casastack_favicon.png');
-
   return (
-    // page overlay, including header and side nav bar
-    <div className={homeStyles.Layout}>
-      <Head>
-        <title>
-          casastack
-        </title>
-        <meta charSet='utf-8' />
-        <link rel="meta icon" href="/casastack_favicon_32_32.png" />
-      </Head>
-
-      <Sidebar
+  // page overlay, including header and side nav bar
+  <div className={homeStyles.Layout}>
+    <Head stackable>
+      <title>casastack</title>
+      <meta charSet="utf-8"/>
+      <link rel="meta icon" href="/casastack_favicon_32_32.png" />
+    </Head>
+    <Sidebar
         as={Menu}
         direction='left'
         icon='labeled'
         inverted
         vertical
         visible
-        width='thin'
+        width="thin"
         position='fixed'
-      >
-        <Link href='/'>
+        stackable
+    >
+        <Link href="/">
           <Menu.Item>
-            <Icon name='home' />
+            <Icon
+              name="home"
+            />
             Home
           </Menu.Item>
         </Link>
-
-        {isCustomer && (
-          <Link href='/joblisting'>
-            <Menu.Item>
-              <Icon name='clipboard' />
-              Listings
-            </Menu.Item>
-          </Link>
-        )}
-
-        {user ? (
-          <>
-            <Link href='/profile'>
-              <Menu.Item>
-                <Icon name='id card' />
-                Profile
-              </Menu.Item>
-            </Link>
-
-            <Menu.Item onClick={handleLogout}>
-              <Icon name='sign-out alternate' />
-              Log Out
-            </Menu.Item>
-          </>
-        ) : (
-          <>
-            <Link href='/login'>
-              <Menu.Item>
-                <Icon name='sign-in alternate' />
-                Log In
-              </Menu.Item>
-            </Link>
-
-            <Link href='/signup'>
-              <Menu.Item>
-                <Icon name='edit' />
-                Sign Up
-              </Menu.Item>
-            </Link>
-          </>
-        )}
-      </Sidebar>
-
+      {isCustomer && (
+        <>
+        <Link href="/joblisting">
+          <Menu.Item>
+            <Icon
+              name="clipboard"
+            />
+            Listings
+          </Menu.Item>
+        </Link>
+        <Link href={`/myListings/volunteer/${user.email}`}>
+          <Menu.Item>
+            <Icon
+              name="eye"
+            />
+            View My Listings
+          </Menu.Item>
+        </Link>
+        <Link href="/rewards">
+          <Menu.Item>
+            <Icon
+              name="certificate"
+            />
+            Rewards
+          </Menu.Item>
+        </Link>
+        </>
+      )}
+      {user ? (
+      <>
+        <Link href="/profile">
+          <Menu.Item>
+            <Icon
+              name="id card"
+            />
+            Profile
+          </Menu.Item>
+        </Link>
+        {!isCustomer ? (
+        <>
+        <Link href="/new">
+          <Menu.Item>
+            <Icon
+              name="pencil alternate"
+            />
+            Create Listing
+          </Menu.Item>
+        </Link>
+        <Link href={`/myListings/user/${user.email}`}>
+          <Menu.Item>
+            <Icon
+              name="eye"
+            />
+            View My Listings
+          </Menu.Item>
+        </Link>
+        </>
+        ) : (null)}
+        <Menu.Item onClick={handleLogout}>
+          <Icon name="sign-out alternate"
+          />
+          Log Out
+        </Menu.Item>
+      </>
+      ) : (
+      <>
+        <Link href="/login">
+          <Menu.Item>
+            <Icon
+              name="sign-in alternate"
+            />
+            Log In
+          </Menu.Item>
+        </Link>
+        <Link href="/signup">
+          <Menu.Item>
+            <Icon
+              name="edit"
+            />
+            Sign Up
+          </Menu.Item>
+        </Link>
+      </>
+      )}
+    </Sidebar>
       <Sidebar.Pusher>
-        <div className={homeStyles.Content}>
-          <div className={homeStyles.Header}>
-            CASASTACK
-          </div>
-          {children}
+      <div className={homeStyles.Content}> 
+        <div className={homeStyles.Header}>
+          Casastack
         </div>
+        {children}
+      </div>
       </Sidebar.Pusher>
-    </div>
+  </div>
   );
 }
-
 export default Layout;
