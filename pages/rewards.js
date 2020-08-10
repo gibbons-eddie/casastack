81d8fa78b235b3f5321f351f0138fb500356d98c
Richard Clarke <ricky.jiangclarke@gmail.com>
2020-08-10 15:09:25 -0400

    Rewards page looks much better

--- pages/rewards.js
+++ pages/rewards.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
-import { Button, Card, Table } from 'semantic-ui-react';
+import { Button, Card, Table, Divider } from 'semantic-ui-react';
 import jobListingStyle from '../components/joblistingsPage/jobListingPageStyles/joblisting.module.css'
 import baseURL from '../utils/baseURL';
 
@@ -28,16 +28,16 @@ const rewards1 = ({ rewards, user }) => {
                 {rewards.map(rewards => {
                     return (
                         <div key={rewards._id}>
-                            <Card style={{marginLeft: 20}}>
+                            <Card style={{marginLeft: 20, maxHeight: 400, minHeight: 400}}>
                                 <Card.Content>
                                     <Card.Header>
 
                                         <Link href={`/rewardInfo/${rewards._id}`}>
-                                            <h1>{rewards.name}</h1>
+                                            <h1 style={{maxHeight: 50, minHeight: 50, fontSize: 25}}>{rewards.name}</h1>
                                         </Link>
-
+                                        <Divider section />
                                         <Link href={`/rewardInfo/${rewards._id}`}>
-                                            <h2>{rewards.condition}</h2>
+                                            <h2 style={{fontSize: 20}}>{rewards.condition}</h2>
                                         </Link>
 
                                     </Card.Header>
